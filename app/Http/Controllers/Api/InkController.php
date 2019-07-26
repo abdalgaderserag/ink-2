a29a6d9d08b30e92308631793599d51f63655f7c
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-26 08:44:53 -0700

    back-end basic scf

---
+++ app/Http/Controllers/Api/InkController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Ink;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Ink  $ink
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ink $ink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Ink  $ink
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ink $ink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Ink  $ink
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ink $ink)
+    {
+        //
+    }
+}
---
+++ database/factories/InkFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Ink::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
