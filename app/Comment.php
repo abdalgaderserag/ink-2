01dc87834a05e1dea09ccc83f0391ea8ea7c8f1c
abdalgaderserag <abdalgadersir@gmail.com>
2019-07-26 11:32:20 -0700

    add the realtions to the models

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany('App\comment');
+    }
+
+    public function media()
+    {
+        return $this->hasOne('App\Media');
+    }
 }
--- app/Ink.php
+++ app/Ink.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ink extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function like()
+    {
+        return $this->hasMany('App\Like');
+    }
+
+    public function comment()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function media()
+    {
+        return $this->hasOne('App\Media');
+    }
 }
